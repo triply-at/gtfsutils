cd66b04d971a2cd3edb33705a7360746cf6238d6
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2022-04-27 16:13:49 +0200

    Refactor to argparse sub-commands

--- gtfsutils/__main__.py
+++ gtfsutils/__main__.py
@@ -2,43 +2,55 @@ import json
 import time
 import logging
 import argparse
-from . import (
-    load_gtfs,
-    save_gtfs,
-    get_bounding_box,
-    print_info
-)
-from .filter import filter_by_geometry
+
+import gtfsutils
+import gtfsutils.filter
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="GTFS Utilities")
-    parser.add_argument(action='store',
-        dest='method', help="GTFS method: filter, bounds, info, merge")
-    parser.add_argument("-i", "--input", action='store',
-        dest='src', help="Input filepath")
-    parser.add_argument("-o", "--output", action='store',
-        dest='dst', help="Output filepath")
-    parser.add_argument("--bounds", action='store',
-        dest='bounds', help="Filter boundary")
-    parser.add_argument("-f", "--filter-operation", action='store',
-        dest='operation', help="Filter operation (within, intersects)",
+    parser = argparse.ArgumentParser(description="GTFS Utilities")
+    subparsers = parser.add_subparsers(dest="method", help="Method")
+
+    # Filter method
+    parser_filter = subparsers.add_parser("filter", help="Filter method")
+    parser_filter.add_argument(dest="src", help="Input GTFS filepath")
+    parser_filter.add_argument(dest="dst", help="Output GTFS filepath")
+    parser_filter.add_argument(dest="bounds", help="Filter boundary")
+    parser_filter.add_argument("-o", "--operation",
+        dest='operation',
+        help="Filter operation (within, intersects)",
         default="within")
-    parser.add_argument("--overwrite", action='store_true',
+    parser_filter.add_argument("--overwrite", action='store_true',
         dest='overwrite', help="Overwrite if exists")
-    parser.add_argument('-v', '--verbose', action='store_true',
+    parser_filter.add_argument('-v', '--verbose', action='store_true',
         dest='verbose', default=False,
         help="Verbose output")
+
+    # Bounds method
+    parser_bounds = subparsers.add_parser("bounds", help="Bounds method")
+    parser_bounds.add_argument(dest="src", help="Input GTFS filepath")
+
+    # Info method
+    parser_info = subparsers.add_parser("info", help="Info method")
+    parser_info.add_argument(dest="src", help="Input GTFS filepath")
+
+    # Version method
+    subparsers.add_parser("version", help="Print version")
+
     args = parser.parse_args()
 
-    log_level = logging.DEBUG if args.verbose else logging.INFO
-    logging.basicConfig(
-        format='%(asctime)s [%(levelname)s] %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        level=log_level)
+    # # Verbose output
+    if  'verbose' in args:
+        log_level = logging.DEBUG if args.verbose else logging.INFO
+        logging.basicConfig(
+            format='%(asctime)s [%(levelname)s] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            level=log_level)
 
-    if args.method == "filter":
+    if args.method == "version":
+        print(f"{gtfsutils.__name__} {gtfsutils.__version__}")
+
+    elif args.method == "filter":
         assert args.bounds is not None, "No bounds defined"
         assert args.src is not None, "No input file specified"
         assert args.dst is not None, "No output file specified"
@@ -48,31 +60,32 @@ def main():
 
         # Load GTFS
         t = time.time()
-        df_dict = load_gtfs(args.src)
+        df_dict = gtfsutils.load_gtfs(args.src)
         duration = time.time() - t
         logging.debug(f"Loaded {args.src} in {duration:.2f}s")
 
         # Filter GTFS
         t = time.time()
-        filter_by_geometry(df_dict, bounds, operation=args.operation)
+        gtfsutils.filter.filter_by_geometry(
+            df_dict, bounds, operation=args.operation)
         duration = time.time() - t
         logging.debug(f"Filtered {args.src} in {duration:.2f}s")
 
         # Save filtered GTFS
         t = time.time()
-        save_gtfs(df_dict, args.dst, ignore_required=True, overwrite=args.overwrite)
+        gtfsutils.save_gtfs(df_dict, args.dst, ignore_required=True, overwrite=args.overwrite)
         duration = time.time() - t
         logging.debug(f"Saved to {args.dst} in {duration:.2f}s")
     
     elif args.method == "bounds":
         assert args.src is not None, "No input file specified"
 
-        bounds = get_bounding_box(args.src)
+        bounds = gtfsutils.get_bounding_box(args.src)
         print(bounds)
     
     elif args.method == "info":
         assert args.src is not None, "No input file specified"
-        print_info(args.src)
+        gtfsutils.print_info(args.src)
 
     elif args.method == "merge":
         raise NotImplementedError("Merge not implemented")
